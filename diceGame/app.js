f6e2a80d3e8247f6dd8637d2bd7c6a5c77f3647f
Gon Espeche <39110211+gonpeche@users.noreply.github.com>
2018-07-01 18:57:20 -0300

    Update app.js

--- diceGame/app.js
+++ diceGame/app.js
@@ -8,3 +8,26 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {  
+    console.log("hola");  
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Display the result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+    
+    // 3. Update the ropund score if the rolled number was not a 1
+    
+});
